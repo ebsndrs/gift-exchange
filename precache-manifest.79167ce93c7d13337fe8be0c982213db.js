ff2ccf5955ca667d75aabea0fd25f86fd69b42d6
Edward Sanders <esas@tobii.com>
2019-12-30 13:39:42 -0500

    Updates

---
+++ precache-manifest.79167ce93c7d13337fe8be0c982213db.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2bdc1cad9f3566b3c2c9a36e9b9a33e4",
+    "url": "/secret-santa/index.html"
+  },
+  {
+    "revision": "07ff4d3ea57db201f288",
+    "url": "/secret-santa/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "eafb97f57a972ed574de",
+    "url": "/secret-santa/static/js/2.53c53771.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/secret-santa/static/js/2.53c53771.chunk.js.LICENSE"
+  },
+  {
+    "revision": "07ff4d3ea57db201f288",
+    "url": "/secret-santa/static/js/main.2497d649.chunk.js"
+  },
+  {
+    "revision": "4d5b6d00017eff157e06",
+    "url": "/secret-santa/static/js/runtime-main.d82b7abf.js"
+  }
+]);
